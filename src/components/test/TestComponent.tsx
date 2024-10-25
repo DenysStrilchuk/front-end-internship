982510ce3c5c872e98d227485508ba522407a04d
Denys Sttrilchuk <denia876@gmail.com>
2024-10-25 13:58:02 +0300

    Installed Axios and created axiosInstance with a base URL, default headers, and interceptors for handling requests and responses. Implemented an endpoint for server health check and the checkStatus function that calls this endpoint. Added the checkServerStatus function in the TestComponent to display server status. Refactored and prepared the code for PR.

--- src/components/test/TestComponent.tsx
+++ src/components/test/TestComponent.tsx
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import { useAppDispatch, useAppSelector } from "../../hooks";
+import {useAppDispatch, useAppSelector} from "../../hooks";
 import {testActions} from "../../store/slices";
+import {checkStatus} from "../../api/status-api";
 
 const TestComponent = () => {
     const testString = useAppSelector((state) => state.test.testString);
     const dispatch = useAppDispatch();
     const [inputValue, setInputValue] = useState<string>('');
+    const [status, setStatus] = useState<{ code: number; detail: string; result: string } | null>(null);
+
+    const checkServerStatus = async () => {
+        try {
+            const status = await checkStatus();
+            setStatus({
+                code: status.status_code,
+                detail: status.detail,
+                result: status.result
+            });
+        } catch (error) {
+            console.error('Failed to check server health:', error);
+            setStatus({
+                code: -1,
+                detail: 'Failed to retrieve status',
+                result: 'Error'
+            });
+        }
+    };
 
     return (
         <div>
@@ -18,8 +38,18 @@ const TestComponent = () => {
             <button onClick={() => dispatch(testActions.setTestString(inputValue))}>
                 Change String
             </button>
+
+            <button onClick={checkServerStatus}>Check Server Health</button>
+
+            {status && (
+                <div>
+                    <p>Server Health Status Code: {status.code}</p>
+                    <p>Detail: {status.detail}</p>
+                    <p>Result: {status.result}</p>
+                </div>
+            )}
         </div>
     );
 };
 
-export { TestComponent };
+export {TestComponent};
---
+++ src/constants/urls.ts
@@ -0,0 +1,14 @@
+const baseUrl = process.env.REACT_APP_API_URL;
+
+const status = '/';
+
+const urls = {
+    status: {
+        base: status
+    }
+}
+
+export {
+    baseUrl,
+    urls
+}
